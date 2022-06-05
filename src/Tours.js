35f8ec6a2720b4b619a859c82ffa65101d9e00df
AMRIT <ca@shahiamrit365.com.np>
2022-06-05 12:01:44 +0545

    added final version

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+
+  return <section>
+    <div className="title">
+      <h2>
+        Our Tours
+      </h2>
+      <div className="underline">
+      </div>
+    </div>
+    <div>
+      {
+        tours.map((person) => {
+          return <Tour key={person.id} {...person} removeTour={removeTour}/>
+        })
+      }
+    </div>
+  </section>;
+ 
 };
 
 export default Tours;
